b1252d3bef4e01b0c7f25e4bba76ba7f4006ce43
Alexandr <vashchekin01@mail.ru>
2021-11-09 14:55:16 +0300

    Первая тестовая версия игры

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
 
 function App() {
+
+  const [isClick, setIsClick] = useState(false);
+
+  // const dino = document.querySelector('.dino');
+  // const cactus = document.querySelector('.cactus');
+
+  document.addEventListener("keydown", (event) => {
+      if(event.code === "Space") {
+          setIsClick(true);
+      }
+
+      setTimeout(() => {
+          setIsClick(false);
+      }, 500);
+  });
+
+  let isAlive = setInterval(() => {
+      let dinoTop = parseInt(window.getComputedStyle(document.querySelector('.dino')).getPropertyValue("bottom"));
+      let cactusLeft = parseInt(window.getComputedStyle(document.querySelector('.cactus')).getPropertyValue("right"))
+      if(cactusLeft < 50 && cactusLeft > 0 && dinoTop <= 50 ){
+          alert("Игра окончена")
+      }
+  },20);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="game">
+          <div className={`dino ${isClick ? 'jump': ''}`} />
+          <div className="cactus" />
+      </div>
+
+
+
     </div>
   );
 }
